e1de38e4c084f676f1fe023040716ff3342bf052
NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-02-11 11:38:44 +0600

    Modified Day by Day

--- Day 01/Palindrome Checker/script.js
+++ Day 01/Palindrome Checker/script.js
@@ -4,13 +4,14 @@ function reverseString(str) {
     return str.split("").reverse().join("")
 }
 
-function palindromCheck(){
+function palindromCheck() {
     const value = input.value;
     const reverse = reverseString(value);
-    if(value === reverse){
+    if (value === reverse) {
         alert("P A L I N D R O M E")
     } else {
         alert("It's Not a palindrom")
     }
+    input.value = "";
 }
 
